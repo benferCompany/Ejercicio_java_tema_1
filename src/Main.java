1c842ee5418974933328d22d9e4e715cbe5523a4
Benjamín Bernabé Gomez <benjidfer@gmail.com>
2023-03-16 16:41:06 -0300

    Ejercicio java tema 1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero = 0;
+        long numero_largo = 1233122;
+        double numero_largo_decimal = 12321132.6465;
+        float numero_decimal = 121321.12f;
+        boolean boleano = true;
+        Integer numero_nulo = null;
+        String hola = "Hola";
+
+        System.out.println(numero);
+        System.out.println(numero_largo);
+        System.out.println(numero_largo_decimal);
+        System.out.println(numero_decimal);
+        System.out.println(boleano);
+        System.out.println(numero_nulo);
+        System.out.println(hola);
+
+
+    }
+}
